18ab70354cb4f81af8a5fbbd3791a90ba7abf04b
AL-ALUMNO <si.bauer@duocuc.cl>
2026-09-03 18:41:31 -0400

    trabajo en js

--- EF1/script.js
+++ EF1/script.js
@@ -6,6 +6,7 @@ document.getElementById("loginform").addEventListener("submit", function(event){
 
     let mensaje = document.getElementById("mensaje");
 
+
     if (mail.match("@")=== null){
         mensaje.innerHTML = '<div class="alert-danger"> Ingrese una direccion de correo valida </div>';
     }
@@ -14,6 +15,40 @@ document.getElementById("loginform").addEventListener("submit", function(event){
         mensaje.innerHTML = '<div class="alert-danger"> Inicio de sesion correcto, redireccionando...</div>';
     }else{
         mensaje.innerHTML = '<div class="alert-danger"> Credenciales no encontradas </div>';
+    }
+
+});
+
+document.getElementById("signupform").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let password = document.getElementById("password").value;
+    let password2 = document.getElementById("password2").value;
+    let mail = document.getElementById("mail").value;
+    let phone = document.getElementById("phone").value;
+    let terminos = document.getElementById("terminos").value;
+
+    let mensaje2 = document.getElementById("mensaje");
+
+    if (nombre || apellido || password || password2 || mail || phone === null){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe rellenar todos los campos </div>';
+    }
+
+    if (mail.match("@")=== null){
+        mensaje2.innerHTML = '<div class="alert-danger"> Ingrese una direccion de correo valida </div>';
+    }
+
+    if (typeof(phone) != Number){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe ingresar un numero valido </div>';
+    }
+
+    if (phone.startsWith(9) === false){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe ingresar un numero valido </div>';
+    }
+    if (phone.lenght > 9 || phone.lenght < 9){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe ingresar un numero valido </div>';
     }
 
 })
