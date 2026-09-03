8163a395599000c0ae8c15afd6f5b0c233b20142
AL-ALUMNO <si.bauer@duocuc.cl>
2026-09-03 18:20:06 -0400

    se creo script inicial de inicio de sesion

---
+++ EF1/script.js
@@ -0,0 +1,19 @@
+document.getElementById("loginform").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let mail = document.getElementById("mail").value;
+    let password = document.getElementById("password").value;
+
+    let mensaje = document.getElementById("mensaje");
+
+    if (mail.match("@")=== null){
+        mensaje.innerHTML = '<div class="alert-danger"> Ingrese una direccion de correo valida </div>';
+    }
+
+    if (mail === "admin@admin.com" && password === "1234"){
+        mensaje.innerHTML = '<div class="alert-danger"> Inicio de sesion correcto, redireccionando...</div>';
+    }else{
+        mensaje.innerHTML = '<div class="alert-danger"> Credenciales no encontradas </div>';
+    }
+
+})
