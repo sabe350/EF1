0e831e47bb7d41845a3d5e5c6cad8270889a088e
sabe350 <si.bauer@duocuc.cl>
2026-09-04 00:53:28 -0400

    agrgado script2 como parche para hacer funcionar script en pagina de registro

---
+++ EF1/script2.js
@@ -0,0 +1,39 @@
+document.getElementById("signupform").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let password = document.getElementById("password").value;
+    let password2 = document.getElementById("password2").value;
+    let mail = document.getElementById("mail").value;
+    let phone = document.getElementById("phone").value;
+    let terminos = document.getElementById("terminos").value;
+
+    let mensaje2 = document.getElementById("mensaje2");
+
+    if (nombre || apellido || password || password2 || mail || phone === null){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe rellenar todos los campos </div>';
+    }else{
+       mensaje2.innerHTML = '<div class="alert-danger"> Registro completado exitosamente </div>'; 
+    }
+
+    if (password2 != password){
+        mensaje2.innerHTML = '<div class="alert-danger"> Contraseñas no coinciden </div>';
+    }
+
+    if (mail.match("@")=== null){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe ingresar una direccion de correo valida </div>';
+    }
+
+    if (phone.startsWith(9) === false){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe ingresar un numero valido </div>';
+    }else if (phone.lenght > 9 || phone.lenght < 9){
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe ingresar un numero valido </div>';
+    }
+
+    if (document.getElementById("terminos").checked){
+
+    }else{
+        mensaje2.innerHTML = '<div class="alert-danger"> Debe aceptar los terminos y condiciones </div>';
+    }
+});
